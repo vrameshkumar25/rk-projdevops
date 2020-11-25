b2507c7692e60d20079aa2fa089bee46b1014aaf
vrameshkumar25 <vrameshkumar25@gmail.com>
2020-11-25 22:14:47 +0530

    Create docker.pp
    docker.pp is for Puppet agent - docker installation in agent.

---
+++ docker.pp
@@ -0,0 +1,23 @@
+class docker {
+       package {'curl':
+               ensure  => present,
+       }
+       exec {'apt-update':
+               command => '/usr/bin/apt-get update'
+       }
+       exec {'download_docker_key':
+               command => '/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+       }
+       exec {'add_docker_repo':
+               command => '/usr/bin/add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
+               require => Exec['apt-update']
+       }
+       exec {'docker_cache':
+               command => '/usr/bin/apt-cache policy docker-ce'
+       }
+       exec {'install_docker':
+               command => '/usr/bin/apt-get install -y docker-ce'
+       }
+}
+
+class{'docker':}
